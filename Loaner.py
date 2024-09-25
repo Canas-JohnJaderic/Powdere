b0c6a7059073bb3e279ef5cbffecebc68308599c
Canas-JohnJaderic <jayjaycanas06@gmail.com>
2024-09-25 18:34:36 +0800

    Loaner.py

---
+++ Loaner.py
@@ -0,0 +1,15 @@
+salary=input("Hello please enter your monthly salary :")
+
+
+if float(salary)>=30000.00:
+    print("You are elligible for the loan!")
+else:
+    print("Unfortunately your salary is too low to avail a loan.")
+
+loan=input("Please Enter the amount that you want to Loan :")
+
+if float(loan)>=float(salary)*10:
+    print("Sorry but the loan is too big")
+else:
+    input("How many months do you want to pay the loan for? :")
+print(float(loan) + float(loan)*.10)
