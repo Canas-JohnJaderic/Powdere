1aea7a82104f5270d8c22e2d4724e6330f7f0246
Canas-JohnJaderic <jayjaycanas06@gmail.com>
2024-09-27 16:13:38 +0800

    Loaner.py

--- Loaner.py
+++ Loaner.py
@@ -11,6 +11,7 @@ loan=input("Please Enter the amount that you want to Loan :")
 
 if float(loan)>=float(salary)*10:
     print("Sorry but the loan is too big")
+    exit()
 else:
     input("How many months do you want to pay the loan for? :")
 print(float(loan) + float(loan)*.10)
